011c7ca0e9596bbad17f75e56ad87d9fefb5b0be
BuRny <gamingburny@gmail.com>
2020-04-09 15:37:57 +0200

    Add todo list test

---
+++ Test2/precache-manifest.b28a410ccacbff75843c558c2d798e3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92bf667f7ea6f13e27b93a6f2e90032f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12480095c42a1a095d17",
+    "url": "/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "43b4a49843ddfd99c8b4",
+    "url": "/static/js/2.f3a36889.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3a36889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12480095c42a1a095d17",
+    "url": "/static/js/main.938c79eb.chunk.js"
+  },
+  {
+    "revision": "8117fdaaea355af31a4a",
+    "url": "/static/js/runtime-main.a6ce9e03.js"
+  }
+]);
